fb906f5a4c79b54109faf946311bd64afd128ee7
FSimas92 <fs6806a@student.american.edu>
2019-03-06 13:55:49 -0500

    Exercise 6

---
+++ exercises/exercise06/Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    //This is how long the ring will move in each direction
+    float moveRate = 3;
+    //We will use this variable to store how much time there is left until
+    //we change directions
+    float moveTimer;
+    //This will be how fast we move the ring each Update
+    float moveSpeed = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Decrement the timer, if it is less than zero, it is time to chenge directions!
+        moveTimer = moveTimer - Time.deltaTime;
+        if (moveTimer < 0)
+        {
+            //Change our move direction by multiplying the speed by -1
+            moveSpeed = moveSpeed * -1;
+
+            //Reset the timer
+            moveTimer = moveRate;
+        }
+
+        //Move the transform of the gameObject using the "Translate" function
+        transform.Translate(moveSpeed, 0, 0);
+    }
+}
---
+++ exercises/exercise06/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float moveSpeed = 12f;
+    float rotateSpeed = 90f;
+    float jumpForce = 0.60f;
+
+    Vector3 startPosition;
+    Quaternion startRotation;
+
+    float gravityModifier = 0.2f;
+
+    float yVelocity = 0;
+    bool previousIsGroundedValue;
+
+    CharacterController cc;
+
+    float camLookAhead = 8.5f;
+    float camFollowBehind = 3.7f;
+    float camFollowAbove = 2.5f;
+
+    public GameObject key;
+    public GameObject door;
+    bool gotkey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = gameObject.GetComponent<CharacterController>();
+
+        previousIsGroundedValue = cc.isGrounded;
+
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+
+        gotkey = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, hAxis * rotateSpeed * Time.deltaTime, 0);
+
+        if (!cc.isGrounded)
+        {
+            yVelocity = yVelocity + Physics.gravity.y * gravityModifier * Time.deltaTime;
+
+            if (Input.GetKeyUp(KeyCode.Space) && yVelocity > 0)
+            {
+                yVelocity = 0;
+            }
+        }
+        else
+        {
+            if (!previousIsGroundedValue)
+            {
+
+                yVelocity = 0;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                yVelocity = jumpForce;
+            }
+        }
+
+        Vector3 amountToMove = transform.forward * vAxis * moveSpeed * Time.deltaTime;
+
+        amountToMove.y = yVelocity;
+
+        cc.Move(amountToMove);
+
+        previousIsGroundedValue = cc.isGrounded;
+
+        Vector3 cameraPosition = transform.position + (Vector3.up * camFollowAbove) + (-transform.forward * camFollowBehind);
+        Camera.main.transform.position = cameraPosition;
+        Camera.main.transform.LookAt(transform.position + transform.forward * camLookAhead);
+
+        if (gotkey == true)
+        {
+            key.transform.position = transform.position + (Vector3.up * camFollowAbove);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Salt"))
+        {
+            Debug.Log("Salt hit");
+            gameObject.transform.position = startPosition;
+            gameObject.transform.rotation = startRotation;
+        }
+
+        if (other.gameObject.CompareTag("Key"))
+        {
+            Debug.Log("Key get");
+            key.transform.position = transform.position + (Vector3.up * camFollowAbove);
+            gotkey = true;
+        }
+
+        if (other.gameObject.CompareTag("Door") && gotkey)
+        {
+            Destroy(door);
+            Destroy(key);
+            gotkey = false;
+        }
+    }
+}
